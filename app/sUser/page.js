1b5a4a71ea9ee19993dbe0fd944d8d618f1d7c77
Ak76024 <akashburnwal46@gmail.com>
2026-04-23 21:37:44 +0530

    add close function too

--- app/sUser/page.js
+++ app/sUser/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import { fetchusersuggestions } from '@/actions/useraction';
@@ -10,6 +10,23 @@ const page = () => {
     const [username, setusername] = useState("");
     const [suggestions, setsuggestions] = useState([]);
 
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (
+                !e.target.closest("#suggestions-container") &&
+                !e.target.closest("input[name='search']")
+            ) {
+                setsuggestions([]);
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
     const searchUser = () => {
         if (!username.trim()) {
             toast.error("Please enter a username to search.");
@@ -19,7 +36,11 @@ const page = () => {
     }
 
     const searchsuggestions = async (uname) => {
-        let data =  await fetchusersuggestions(uname.trim());
+        if (!uname.trim()) {
+            setsuggestions([]);
+            return;
+        }
+        let data = await fetchusersuggestions(uname.trim());
         setsuggestions(data);
     }
     return (
@@ -27,7 +48,7 @@ const page = () => {
             <h1 className='text-3xl font-bold'>Search User</h1>
             <div className='w-full lg:w-[40%] flex flex-col relative items-center'>
                 <input className='border border-white p-5 py-2 lg:w-[100%] w-[80%]' type="text" name='search' placeholder='Search for a user...' value={username} onChange={(e) => { setusername(e.target.value); searchsuggestions(e.target.value); }} />
-                <div className='flex flex-col gap-2 absolute top-10 items-center mt-2 w-full'>
+                <div id="suggestions-container" className='flex flex-col gap-2 absolute top-10 items-center mt-2 w-full'>
                     {suggestions.length > 0 && suggestions.map((suggestion, index) => (
                         <div key={index} onClick={() => router.push(`/user/${suggestion.username}`)} className='flex cursor-pointer bg-black items-center gap-2 lg:w-[80%] w-[80%] border border-white p-2'>
                             <img src={suggestion.img} alt={suggestion.username} className='w-8 h-8 rounded-full' />
